f8da53ebacaf11317267b72b38785e5efa17cadb
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-08-27 23:39:46 +0900

    impl CRUD routing

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ use warp::{reject::Reject, Filter, Rejection, Reply};
 #[derive(Clone)]
 struct Store {
     questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
+    answers: Arc<RwLock<HashMap<AnswerId, Answer>>>,
 }
 
 impl Store {
     fn new() -> Self {
         Store {
             questions: Arc::new(RwLock::new(Self::init())),
+            answers: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
@@ -24,6 +26,8 @@ impl Store {
         serde_json::from_str(file).expect("can't read questions.json")
     }
 }
+#[derive(Debug, Serialize, Eq, Clone, PartialEq, Hash, Deserialize)]
+struct QuestionId(String);
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Question {
@@ -33,8 +37,15 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug, Serialize, Eq, Clone, PartialEq, Hash, Deserialize)]
-struct QuestionId(String);
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+struct AnswerId(String);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Answer {
+    id: AnswerId,
+    content: String,
+    question_id: QuestionId,
+}
 
 #[derive(Debug)]
 struct InvalidId;
@@ -50,6 +61,7 @@ struct Pagination {
 enum Error {
     ParseError(std::num::ParseIntError),
     MissingParameters,
+    QuestionNotFound,
 }
 
 impl std::fmt::Display for Error {
@@ -57,10 +69,12 @@ impl std::fmt::Display for Error {
         match &self {
             Error::ParseError(ref err) => write!(f, "Cannot parse parameter: {}", err),
             Error::MissingParameters => write!(f, "Missing parameter"),
+            Error::QuestionNotFound => write!(f, "Question not found"),
         }
     }
 }
 
+// INFO: warpでカスタムエラーを返せるように、Rejectマーカートレイトを実装
 impl Reject for Error {}
 
 fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
@@ -98,6 +112,13 @@ async fn get_questions(
     }
 }
 
+async fn get_question(id: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().get(&QuestionId(id)) {
+        Some(q) => Ok(warp::reply::json(&q)),
+        None => Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+}
+
 async fn add_question(
     question: Question,
     store: Store,
@@ -108,6 +129,43 @@ async fn add_question(
         .insert(question.id.clone(), question);
 
     Ok(warp::reply::with_status("Question added", StatusCode::OK))
+}
+
+async fn update_question(
+    id: String,
+    question: Question,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().get_mut(&QuestionId(id)) {
+        Some(q) => *q = question,
+        None => return Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+
+    Ok(warp::reply::with_status("Question updated", StatusCode::OK))
+}
+
+async fn delete_question(id: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().remove(&QuestionId(id)) {
+        Some(_) => Ok(warp::reply::with_status("Question deleted", StatusCode::OK)),
+        None => Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+}
+
+async fn add_answer(
+    store: Store,
+    params: HashMap<String, String>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let answer = Answer {
+        id: AnswerId("1".to_string()), // TODO: ランダムな数値を生成する
+        content: params.get("content").unwrap().to_string(), // TODO: エラーチェックの追加
+        question_id: QuestionId(params.get("questionId").unwrap().to_string()),
+    };
+
+    // TODO: 質問が存在するかチェック
+
+    store.answers.write().insert(answer.id.clone(), answer);
+
+    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -136,12 +194,11 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
-    // TODO: Store構造体をDBモックとする
+    // INFO: Store構造体をDBモックとする
     let store = Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
     // CORS
-    // TODO: CORSによるエラーはここで落としてくれるので、`return_error`で別途自前で用意する必要はなさそう
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
@@ -155,6 +212,14 @@ async fn main() {
         .and(store_filter.clone()) // 第2引数にStore
         .and_then(get_questions);
 
+    // GET /questions/:question_id
+    let get_question = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(get_question);
+
     // POST /questions
     let add_question = warp::post()
         .and(warp::path("questions"))
@@ -163,8 +228,38 @@ async fn main() {
         .and(store_filter.clone()) // 第2引数にStore
         .and_then(add_question);
 
+    // PUT /questions/:question_id
+    let update_question = warp::put()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .and(store_filter.clone())
+        .and_then(update_question);
+
+    // DELETE /questions/:question_id
+    let delete_question = warp::delete()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(delete_question);
+
+    // POST /answers (x-www-form-urlencoded)
+    // INFO: /questions/:question_id/answers にルートを変更
+    let add_answer = warp::post()
+        .and(warp::path("answers"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::form())
+        .and_then(add_answer);
+
     let routes = get_questions
+        .or(get_question)
         .or(add_question)
+        .or(update_question)
+        .or(delete_question)
+        .or(add_answer)
         .with(cors)
         .recover(return_error);
 
