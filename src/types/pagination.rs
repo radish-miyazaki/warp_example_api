97111d43738e2e597e447a96cb276e87aaa6c660
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-08-30 23:01:12 +0900

    add docs to pagination.rs

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -2,12 +2,29 @@ use std::collections::HashMap;
 
 use handle_errors::Error;
 
+/// Pagination struct which is getting extract from query params
 #[derive(Debug)]
 pub struct Pagination {
+    /// The index of the first item which has to be returned
     pub start: usize,
+    /// The index of the last item which has to be returned
     pub end: usize,
 }
 
+/// Extract query parameters from the `/question` route
+/// # Example query
+/// GET requests to this route can have a pagination attached so we just
+/// return the questions we need
+/// `/questions?start=1&end=10`
+/// # Example usage
+/// ```rust
+/// let mut query = HashMap::new();
+/// query.insert("start".to_string(), "1".to_string());
+/// query.insert("end".to_string(), "10".to_string());
+/// let p = types::pagination::extract_pagination(query).unwrap();
+/// assert_eq!(p.start, 1);
+/// assert_eq!(p.end, 10);
+/// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagination {
