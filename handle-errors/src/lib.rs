94c28dd7ea42bd14c863d1e5cb7c22991a31ac0d
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-09-06 19:40:57 +0900

    必要以上にユーザに情報を渡さないよう修正

--- handle-errors/src/lib.rs
+++ handle-errors/src/lib.rs
@@ -1,3 +1,4 @@
+use tracing::{event, instrument, Level};
 use warp::body::BodyDeserializeError;
 use warp::cors::CorsForbidden;
 use warp::{http::StatusCode, reject::Reject, Rejection, Reply};
@@ -6,8 +7,7 @@ use warp::{http::StatusCode, reject::Reject, Rejection, Reply};
 pub enum Error {
     ParseError(std::num::ParseIntError),
     MissingParameters,
-    QuestionNotFound,
-    DatabaseQueryError(sqlx::Error),
+    DatabaseQueryError,
 }
 
 impl std::fmt::Display for Error {
@@ -15,8 +15,7 @@ impl std::fmt::Display for Error {
         match &self {
             Error::ParseError(ref err) => write!(f, "Cannot parse parameter: {}", err),
             Error::MissingParameters => write!(f, "Missing parameter"),
-            Error::QuestionNotFound => write!(f, "Question not found"),
-            Error::DatabaseQueryError(err) => write!(f, "Query could not be executed: {}", err),
+            Error::DatabaseQueryError => write!(f, "Query could not be executed"),
         }
     }
 }
@@ -24,23 +23,34 @@ impl std::fmt::Display for Error {
 // INFO: warpでカスタムエラーを返せるように、Rejectマーカートレイトを実装
 impl Reject for Error {}
 
+#[instrument]
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(error) = r.find::<Error>() {
+    if let Some(crate::Error::DatabaseQueryError) = r.find() {
+        event!(Level::ERROR, "Database query error");
         Ok(warp::reply::with_status(
-            error.to_string(),
-            StatusCode::RANGE_NOT_SATISFIABLE,
+            crate::Error::DatabaseQueryError.to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else if let Some(error) = r.find::<CorsForbidden>() {
+        event!(Level::ERROR, "CORS forbidden error: {}", error);
         Ok(warp::reply::with_status(
             error.to_string(),
             StatusCode::FORBIDDEN,
         ))
     } else if let Some(error) = r.find::<BodyDeserializeError>() {
+        event!(Level::ERROR, "Cannot deserialize request body: {}", error);
         Ok(warp::reply::with_status(
             error.to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
+    } else if let Some(error) = r.find::<Error>() {
+        event!(Level::ERROR, "{}", error);
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
     } else {
+        event!(Level::WARN, "Requested route was not found");
         Ok(warp::reply::with_status(
             "Route not found".to_string(),
             StatusCode::NOT_FOUND,
