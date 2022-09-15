0ca9717139bc7e3bd82f4530b055eb51d695f193
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-09-16 00:33:44 +0900

    統合テスト追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![warn(clippy::all)]
-use handle_errors::return_error;
+pub use handle_errors;
+
+use tokio::sync::{oneshot, oneshot::Sender};
 use tracing_subscriber::fmt::format::FmtSpan;
 use warp::hyper::Method;
 use warp::Filter;
@@ -108,7 +110,7 @@ async fn build_routes(store: store::Store) -> impl Filter<Extract = impl warp::R
         .or(login)
         .with(cors)
         .with(warp::trace::request())
-        .recover(return_error)
+        .recover(handle_errors::return_error)
 }
 
 pub async fn setup_store(config: &config::Config) -> Result<store::Store, handle_errors::Error> {
@@ -152,4 +154,27 @@ pub async fn setup_store(config: &config::Config) -> Result<store::Store, handle
 pub async fn run(config: config::Config, store: store::Store) {
     let routes = build_routes(store).await;
     warp::serve(routes).run(([0, 0, 0, 0], config.port)).await;
+}
+
+pub struct OneshotHandler {
+    pub sender: Sender<i32>,
+}
+
+/// 統合テスト用に瞬間的に本番と同じ環境のサーバを立ち上げる関数
+pub async fn oneshot(store: store::Store) -> OneshotHandler {
+    let routes = build_routes(store).await;
+    let (tx, rx) = oneshot::channel::<i32>();
+
+    let socket: std::net::SocketAddr = "127.0.0.1:3030"
+        .to_string()
+        .parse()
+        .expect("Not a valid address");
+
+    let (_, server) = warp::serve(routes).bind_with_graceful_shutdown(socket, async {
+        rx.await.ok();
+    });
+
+    tokio::task::spawn(server);
+
+    OneshotHandler { sender: tx }
 }
